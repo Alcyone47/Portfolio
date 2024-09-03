b90e92a4800991b95388d643f4e90abdde3cef72
Alcyone47 <91411013+Alcyone47@users.noreply.github.com>
2024-09-03 16:48:48 +0530

    updated projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-import styles from "./Projects.module.css"
+import styles from "./Projects.module.css";
+
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <ul className="projects">
-        <li>Porject 1</li>
-        <li>Porject 2</li>
-        <li>Porject 3</li>
-      </ul>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
     </section>
   );
 };
